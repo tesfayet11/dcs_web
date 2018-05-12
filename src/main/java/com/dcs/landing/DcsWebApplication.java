1fad7a5c5f30feaa70955191b2d19b311c49abea
Tesfaye Weld. <me@my.com>
2018-05-12 15:02:29 -0400

    Initial Commit

---
+++ src/main/java/com/dcs/landing/DcsWebApplication.java
@@ -0,0 +1,12 @@
+package com.dcs.landing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcsWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcsWebApplication.class, args);
+	}
+}
